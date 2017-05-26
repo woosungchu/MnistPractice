8123799f317272b64519c13d0dc6b7a190942089
woosungchu <woosungchu@gmail.com>
2017-05-26 20:49:57 +0900

    First init

---
+++ README.md
@@ -0,0 +1,22 @@
+## MNIST Practice
+
+
+#### SETUP
+```
+npm install
+npm install brain.js
+npm install mnist
+node train.js
+```
+
+#### TEST
+```
+node simpleRecognize.js
+// or
+nnTest.html
+```
+
+
+#### Reference
+
+- [Origin Source](https://github.com/ApelSYN/mnist_brain)
---
+++ train.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const brain = require('brain.js');
+var net = new brain.NeuralNetwork();
+const fs = require('fs');
+
+const mnist = require('mnist');
+
+const set = mnist.set(20000, 0);
+
+const trainingSet = set.training;
+//const testSet = set.test;
+
+net.train(trainingSet,
+    {
+            errorThresh: 0.001,  // error threshold to reach
+            iterations: 2,   // maximum training iterations
+            log: true,           // console.log() progress periodically
+            logPeriod: 1,       // number of iterations between logging
+            learningRate: 0.3    // learning rate
+    }
+);
+
+
+let wstream = fs.createWriteStream('./data/mnistTrain.json');
+wstream.write(JSON.stringify(net.toJSON(),null,2));
+wstream.end();
+
+console.log('MNIST dataset with Brain.js train done.')
